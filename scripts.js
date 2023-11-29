52166bbade155092dbc88ce8dac0ae222593e806
David <daviddanielc25@gmail.com>
2023-11-29 18:34:28 -0300

    Portfolio Web Init

---
+++ scripts.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+
+    /** NAVBAR EVENTS */
+    const navbar = document.querySelector(".navbar_")
+    const botonAbrirNavbar = navbar.querySelector(".abrir_")
+    const opcionesNavbar = navbar.querySelector("ul")
+
+    botonAbrirNavbar.addEventListener("click", function(){
+        botonAbrirNavbar.classList.toggle("hidden_")
+        opcionesNavbar.classList.toggle("show-navbar_")
+    })
+    
+
+
+
+
+
+
+})
